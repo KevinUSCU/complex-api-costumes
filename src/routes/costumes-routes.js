9ef3178c671842c6d42da9e3b2ac3e6bd45ee690
Kevin Springer <KevinUSCU@outlook.com>
2017-10-21 11:54:34 -0700

    Update routes to name costume id and tag id parameters uniquely.

--- src/routes/costumes-routes.js
+++ src/routes/costumes-routes.js
@@ -4,17 +4,17 @@ const ctrl = require('../controllers/costumes-controllers')
 
 // Routes for Costumes
 router.get('/', ctrl.getAllCostumes)
-router.get('/:id', ctrl.getCostume)
+router.get('/:costumeId', ctrl.getCostume)
 router.post('/', ctrl.createCostume)
 router.post('/random/:number', ctrl.randomCostumes)
-router.put('/:id', ctrl.updateCostume)
-router.delete('/:id', ctrl.deleteCostume)
+router.put('/:costumeId', ctrl.updateCostume)
+router.delete('/:costumeId', ctrl.deleteCostume)
 
 // Routes for Tags
-router.get('/tags', ctrl.getCostumeTags)
-router.get('/tags/:id', ctrl.getCostumeTag)
-router.post('/tags', ctrl.createCostumeTag)
-router.put('/tags/:id', ctrl.updateCostumeTag)
-router.delete('/tags/:id', ctrl.deleteCostumeTag)
+router.get('/:costumeId/tags', ctrl.getCostumeTags)
+router.get('/:costumeId/tags/:tagId', ctrl.getCostumeTag)
+router.post('/:costumeId/tags', ctrl.createCostumeTag)
+router.put('/:costumeId/tags/:tagId', ctrl.updateCostumeTag)
+router.delete('/:costumeId/tags/:tagId', ctrl.deleteCostumeTag)
 
 module.exports = router
