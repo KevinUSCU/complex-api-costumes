983466ce531838a9f64cd2f3f782f5fe1f5ca008
Kevin Springer <KevinUSCU@outlook.com>
2017-10-23 15:30:40 -0700

    Fix app.disable typo.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-app.disable('x-server-by')
+app.disable('x-powered-by')
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
